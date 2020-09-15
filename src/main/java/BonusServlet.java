4227559187bc8c23c4f09419329a2ea8012e3ab1
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-09-15 15:31:32 -0500

    done

--- src/main/java/BonusServlet.java
+++ src/main/java/BonusServlet.java
@@ -18,12 +18,3 @@ public class BonusServlet extends HttpServlet {
         out.println("<h1> Hello, my name is " + firstName + lastName + "  </h1>");
     }
 }
-
-
-
-
-//        4. Create a servlet that listens for requests to ```/ping``` and a servlet that listens for requests to
-//        ```/pong```. The former servlet should simply return a link that sends a request to the latter, and vice versa!
-//        If successfully built, the user should be able to cycle back and forth between two pages by clicking on
-//        a ```PING!``` or ```PONG!``` link.
-
